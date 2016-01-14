7194ed8b11c9fe623168a0656b78af06526ee55a
Jean-Louis Fuchs <ganwell@fangorn.ch>
2016-01-14 23:38:08 +0100

    Searching and search modes

--- finja.py
+++ finja.py
@@ -13,15 +13,20 @@ _shlex_settings = {
     }
 }
 
+_ignore_dir = set([
+    ".git",
+    ".svn"
+])
 
-def get_db(create=False, path="FINJA"):
+
+def get_db(create=False):
     global _connection
     if _connection:
         return _connection  # noqa
-    exists = os.path.exists(path)
+    exists = os.path.exists("FINJA")
     if not (create or exists):
         raise ValueError("Could not find FINJA")
-    _connection = sqlite3.connect(path)  # noqa
+    _connection = sqlite3.connect("FINJA")  # noqa
     if not exists:
         _connection.execute("""
             CREATE TABLE
@@ -48,19 +53,19 @@ def get_db(create=False, path="FINJA"):
     return _connection
 
 
-def apply_shlex_settings(ext, lex):
-    to_apply = [_shlex_settings['.default']]
-    if ext in _shlex_settings:
-        to_apply.append(_shlex_settings[ext])
+def apply_shlex_settings(shlex_settings, ext, lex):
+    to_apply = [shlex_settings['.default']]
+    if ext in shlex_settings:
+        to_apply.append(shlex_settings[ext])
     for settings in to_apply:
         for key in settings.keys():
             setattr(lex, key, settings[key])
 
 
-def index_file(con, file_path):
-    sys.stdout.write("%s%s" % (file_path, ": "))
+def index_file(con, file_path, update=False):
     if is_binary(file_path):
-        print("is binary, skipping")
+        if not update:
+            print("%s: is binary, skipping" % (file_path,))
         return
     else:
         mode = os.stat(file_path)
@@ -79,14 +84,29 @@ def index_file(con, file_path):
                 old_inode = res[0][0]
         if old_inode != inode:
             with open(file_path, "r") as f:
-                inserts = []
-                lex = shlex.shlex(f, file_path)
-                ext = file_path.split(os.path.extsep)[-1]
-                apply_shlex_settings(ext, lex)
-                t = lex.get_token()
-                while t:
-                    inserts.append((t, file_path, lex.lineno))
-                    t = lex.get_token()
+                retry = 0
+                shlex_settings = dict(_shlex_settings)
+                while retry <= 1:
+                    try:
+                        inserts = []
+                        lex = shlex.shlex(f, file_path)
+                        ext = file_path.split(os.path.extsep)[-1]
+                        apply_shlex_settings(
+                            shlex_settings,
+                            ext,
+                            lex
+                        )
+                        t = lex.get_token()
+                        while t:
+                            inserts.append((t, file_path, lex.lineno))
+                            t = lex.get_token()
+                        break
+                    except ValueError:
+                        if retry == 0:
+                            shlex_settings['.default']['quotes'] = ""
+                        else:
+                            raise
+                    retry += 1
             with con:
                 con.execute("""
                     DELETE FROM
@@ -94,30 +114,44 @@ def index_file(con, file_path):
                     WHERE
                         file=?;
                 """, (file_path,))
-                con.executemany("""
-                    INSERT INTO
-                        finja(token, file, line)
-                    VALUES
-                        (?, ?, ?);
-                """, inserts)
+                try:
+                    con.executemany("""
+                        INSERT INTO
+                            finja(token, file, line)
+                        VALUES
+                            (?, ?, ?);
+                    """, inserts)
+                except sqlite3.ProgrammingError:
+                    if not update:
+                        print("%s: is binary, skipping (late)" % (file_path,))
+                    return
                 con.execute("""
                     INSERT OR REPLACE INTO
                         file(path, inode)
                     VALUES
                         (?, ?);
                 """, (file_path, inode))
-        print("ok")
+            print("%s: indexed" % (file_path,))
+        else:
+            if not update:
+                print("%s: uptodate" % (file_path,))
 
 
 def index():
     con = get_db(create=True)
+    do_index(con)
+
+
+def do_index(con, update=False):
     for dirpath, _, filenames in os.walk("."):
+        if set(dirpath.split(os.sep)).intersection(_ignore_dir):
+            continue
         for filename in filenames:
             file_path = os.path.join(
                 dirpath,
                 filename
             )
-            index_file(con, file_path)
+            index_file(con, file_path, update)
     con.execute("VACUUM;")
 
 
@@ -127,15 +161,53 @@ def find_finja():
     while lcwd:
         cwd = os.sep.join(lcwd)
         check = os.path.join(cwd, "FINJA")
-        if os.path.exists(check):
-            return check
+        if os.path.isfile(check):
+            return cwd
         lcwd.pop()
     raise ValueError("Could not find FINJA")
 
 
-def search(search):
+def search(
+        search,
+        file_mode=False,
+        update=False,
+):
     finja = find_finja()
-    con = get_db(create=False, path=finja)
+    os.chdir(finja)
+    con = get_db(create=False)
+    if update:
+        do_index(con, update=True)
+    res = []
+    with con:
+        if file_mode:
+            for word in search:
+                res.append(set(con.execute("""
+                    SELECT DISTINCT
+                        file
+                    FROM
+                        finja
+                    WHERE
+                        token=?
+                """, (word,)).fetchall()))
+        else:
+            for word in search:
+                res.append(set(con.execute("""
+                    SELECT DISTINCT
+                        file,
+                        line
+                    FROM
+                        finja
+                    WHERE
+                        token=?
+                """, (word,)).fetchall()))
+    res_set = res.pop()
+    for search_set in res:
+        res_set.intersection_update(search_set)
+    for match in res_set:
+        path = match[0]
+        if path.startswith("./"):
+            path = path[2:]
+        print("%s:%s" % (path, match[1]))
 
 
 def main(argv=None):
@@ -150,6 +222,18 @@ def main(argv=None):
         action='store_true',
     )
     parser.add_argument(
+        '--update',
+        '-u',
+        help='Update the index before searching',
+        action='store_true',
+    )
+    parser.add_argument(
+        '--file-mode',
+        '-f',
+        help='Ignore line-number when matching search strings',
+        action='store_true',
+    )
+    parser.add_argument(
         'search',
         help='search string',
         type=str,
@@ -160,4 +244,8 @@ def main(argv=None):
     if args.index:
         index()
     if args.search:
-        search(args.search)
+        search(
+            args.search,
+            file_mode=args.file_mode,
+            update=args.update
+        )
